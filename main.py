b6bd5b49c711e00923f5a627ae75783ec749e203
진대윤 <zetbouaka@gmail.com>
2022-07-09 10:02:43 +0000

    deploy test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Heroku test"}
+    return {"message": "배포 성공!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
